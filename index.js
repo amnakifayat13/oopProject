e3d651fe895831fc00de40957a3b1c37b0d12646
amnakifayat13 <hayatullahamna@gmail.com>
2024-05-26 14:52:23 +0500

    oop Project

---
+++ index.js
@@ -0,0 +1,49 @@
+#! /usr/bin/env node
+import inquirer from "inquirer";
+class Student {
+    name;
+    constructor(n) {
+        this.name = n;
+    }
+}
+class Person {
+    students = [];
+    addStudent(obj) {
+        this.students.push(obj);
+    }
+}
+const persons = new Person;
+const programStart = async (persons) => {
+    do {
+        console.log("Welcome");
+        const answer = await inquirer.prompt({
+            type: "list",
+            message: "To whom would you like to talk?",
+            choices: ["Self", "student"],
+            name: "select",
+        });
+        if (answer.select == "Self") {
+            console.log("I am talking with myself");
+            console.log("My health is good");
+        }
+        if (answer.select == "student") {
+            const ans = await inquirer.prompt({
+                type: "input",
+                message: "To Whom Student do want to talk?",
+                name: "student",
+            });
+            const student = persons.students.find(val => val.name == ans.student);
+            if (!student) {
+                const name = new Student(ans.student);
+                persons.addStudent(name);
+                console.log(`Hello I am ${name.name} and I am good`);
+                console.log(persons.students);
+            }
+            if (student) {
+                console.log(`Hello I am ${student.name} and I am good.........................`);
+                console.log(persons.students);
+            }
+        }
+    } while (true);
+};
+programStart(persons);
